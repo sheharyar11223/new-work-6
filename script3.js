44f1ee9bf0e9b8d256431e12bdfa232d8830da9a
sheharyar11223 <sheharyarmahar37@gmail.com>
2026-07-01 17:03:56 +0500

    task done

---
+++ script3.js
@@ -0,0 +1,13 @@
+let name = "Sheharyar";
+
+let last_name = "Khan";
+
+let city = "Ghotki";
+
+let profession = "Web developer";
+
+let Class = 10;
+
+let village = "Sheroo mahar";
+
+console.log(`My name is ${name} ${last_name} from ${city} and I live in ${village} and I am a ${profession} in class ${Class}.`);
